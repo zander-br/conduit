093a071c7e623839c76cd32cc698517dc52be6b8
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-11-04 06:47:40 -0300

    feat: add user aggregate

---
+++ lib/conduit/accounts/aggregates/user.ex
@@ -0,0 +1,28 @@
+defmodule Conduit.Accounts.Aggregates.User do
+  defstruct [:uuid, :username, :email, :hashed_password]
+
+  alias Conduit.Accounts.Aggregates.User
+
+  @doc """
+    Register a new user
+  """
+
+  def execute(%User{uuid: nil}, %RegisterUser{} = register) do
+    %UserRegistered{
+      user_uuid: register.user_uuid,
+      username: register.username,
+      email: register.email,
+      hashed_password: register.hashed_password
+    }
+  end
+
+  def apply(%User{} = user, %UserRegistered{} = registered) do
+    %User{
+      user
+      | uuid: registered.user_uuid,
+        username: register.username,
+        email: register.email,
+        hashed_password: register.hashed_password
+    }
+  end
+end
