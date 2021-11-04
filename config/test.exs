f048aba3efafd4c3bcf3af790c0841a7e6dfee31
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-11-04 05:44:33 -0300

    chore: initial commit

---
+++ config/test.exs
@@ -0,0 +1,27 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :conduit, Conduit.Repo,
+  username: "postgres",
+  password: "postgres",
+  database: "conduit_test#{System.get_env("MIX_TEST_PARTITION")}",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :conduit, ConduitWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "YhB9w3T8bW8Epc1a7mcMiEwNRbbpKqa2dlLcN8Ce9AI1idaNU4KBERM2IqjhcwXq",
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
