92059aa7fe1c3f86ef6deefa806354d7e16b58b7
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-11-18 08:27:24 -0300

    feat: add register user validates

--- lib/conduit/accounts/commands/register_user.ex
+++ lib/conduit/accounts/commands/register_user.ex
@@ -1,5 +1,11 @@
 defmodule Conduit.Accounts.Commands.RegisterUser do
-  defstruct [:user_uuid, :username, :email, :password, :hashed_password]
+  defstruct [:user_uuid, :username, :email, :hashed_password]
 
   use ExConstructor
+  use Vex.Struct
+
+  validates(:user_uuid, uuid: true)
+  validates(:username, presence: true, string: true)
+  validates(:email, presence: true, string: true)
+  validates(:hashed_password, presence: true, string: true)
 end
