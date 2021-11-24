dc461f92e4cb0c4e6d0adc43152ca301c4bb868a
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-11-24 09:09:59 -0300

    feat: filter by author

--- lib/conduit/blog/queries/list_articles.ex
+++ lib/conduit/blog/queries/list_articles.ex
@@ -4,7 +4,7 @@ defmodule Conduit.Blog.Queries.ListArticles do
   alias Conduit.Blog.Projections.Article
 
   defmodule Options do
-    defstruct limit: 20, offset: 0
+    defstruct limit: 20, offset: 0, author: nil
 
     use ExConstructor
   end
@@ -12,14 +12,21 @@ defmodule Conduit.Blog.Queries.ListArticles do
   def paginate(params, repo) do
     options = Options.new(params)
 
-    articles = query() |> entries(options) |> repo.all()
-    total_count = query() |> count() |> repo.aggregate(:count, :id)
+    articles = query(options) |> entries(options) |> repo.all()
+    total_count = query(options) |> count() |> repo.aggregate(:count, :id)
 
     {articles, total_count}
   end
 
-  defp query do
+  defp query(options) do
     from(a in Article)
+    |> filter_by_author(options)
+  end
+
+  defp filter_by_author(query, %Options{author: nil}), do: query
+
+  defp filter_by_author(query, %Options{author: author}) do
+    query |> where(author_username: ^author)
   end
 
   defp entries(query, %Options{limit: limit, offset: offset}) do
